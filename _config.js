74c9369e2db2a6f91cea0832ded32e7cf98a9916
hao <haleluohao@gmail.com>
2019-09-29 21:27:37 +0100

    updated the stats

--- _config.js
+++ _config.js
@@ -3,10 +3,10 @@ const Config = {
     url: "https://www.sporule.com",
     description: "Sporule is a micro blog site",
     keywords: "micro blog",
-    logo: "https://i.imgur.com/MrRLOC4.png",
-    disqusShortname: "yourdisqusshortname",
+    logo: "https://avatars1.githubusercontent.com/u/3833026?s=460&v=4",
+    disqusShortname: "sporule",
     postPerPage: 8,
-    googleAnaltics: "",
+    googleAnaltics: "UA-114032457-1",
     alwaysRefreshPost: false,
     gh_custom_domain: true
 
