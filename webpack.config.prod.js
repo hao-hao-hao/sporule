9f57031f243b56421244f13ae8f84f3d6925036b
Sporule <hao@sporule.com>
2019-12-22 21:05:02 +0000

    updated template

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -134,7 +134,7 @@ module.exports = {
         events: true
       },
       responseStrategy: 'cache-first',
-      excludes: ['**/.*', '**/*.map', '**/*.gz', '**/*.xml', '**/*.txt', '**/*.md', '**/_redirects'],
+      excludes: ['**/.*', '**/*.map', '**/*.gz', '**/*.xml', '**/*.txt'],
       autoUpdate: 1000 * 60 * 60 * 10,
       externals: [
         'https://cdn.jsdelivr.net/npm/pwacompat@2.0.7/pwacompat.min.js',
