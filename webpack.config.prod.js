1d97b3210fbdb47026715ee0793a99ca4c48c495
Hao <hao@sporule.com>
2020-04-13 15:28:13 +0100

    removed external

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -139,11 +139,11 @@ module.exports = {
       responseStrategy: 'cache-first',
       excludes: ['**/*.*','**/.*', '**/*.map','**/*.md','**/*.gz', '**/*.txt', '**/sw.js', '**/netlify.toml', '**/*.jpg', '**/*.png', '**/*.gif', '**/*.jpeg', "**/CNAME",'**/*.xml', '**/*.txt'],
       autoUpdate: 1000 * 60 * 60 * 10,
-      externals: [
-        'https://cdn.jsdelivr.net/npm/pwacompat@2.0.7/pwacompat.min.js',
-        'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.slim.min.js',
-        'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
-      ],
+      // externals: [
+      //   'https://cdn.jsdelivr.net/npm/pwacompat@2.0.7/pwacompat.min.js',
+      //   'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.slim.min.js',
+      //   'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
+      // ],
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
