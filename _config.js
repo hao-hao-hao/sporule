a0d3d57792a44f988c14890c4790b9974bf436a7
Hao <hao@sporule.com>
2020-04-14 20:28:25 +0100

    sync from origin

--- _config.js
+++ _config.js
@@ -1,13 +1,16 @@
 const Config = {
     site: "Sporule",
-    subTitle:"micro blog",
+    subTitle: "SubTitle",
     url: "https://www.sporule.com",
     description: "Sporule is a micro blog site",
     keywords: "micro blog",
     logo: "https://i.imgur.com/1UDpSHj.png",
     disqusShortname: "sporule",
     postPerPage: 8,
-    googleAnaltics: "UA-114032457-1",
+    email: "hao@sporule.com",
+    language:'en',
+    internalLinkNames: ["sporule"],
+    googleAnaltics: "",
     alwaysRefreshPost: false,
     gh_custom_domain: true
 
