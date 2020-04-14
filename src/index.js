19c37e6ce78dfcf8ee8b9396b7ec21ee259451c6
Hao <hao@sporule.com>
2020-04-14 22:28:44 +0100

    fixed small bug

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ else if ('serviceWorker' in navigator) {
 setTimeout(() => {
     //initial load
     const states = store.getState();
-    if (states.posts.items[0].title != "") {
+    if (states.posts.items[0].title == "") {
         store.dispatch(PostAction.loadPosts());
     }
 }, 1000)
